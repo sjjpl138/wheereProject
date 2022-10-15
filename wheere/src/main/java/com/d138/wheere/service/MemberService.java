d7374a4810bff3edcba58ca649a3c20f7277d4d6
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-10-16 00:10:40 +0900

    MemberService 생성

--- wheere/src/main/java/com/d138/wheere/service/MemberService.java
+++ wheere/src/main/java/com/d138/wheere/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.d138.wheere.service;
 
+import com.d138.wheere.domain.Member;
 import com.d138.wheere.domain.Reservation;
 import com.d138.wheere.repository.MemberRepository;
 import com.d138.wheere.repository.ReservationRepository;
@@ -17,4 +18,21 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     private final ReservationRepository reservationRepository;
+
+    @Transactional
+    public Long saveMember(Member member) {
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    @Transactional
+    public void modifyPhoneNumber(Long memberId, String phoneNumber) {
+        Member findMember = memberRepository.findOne(memberId);
+        findMember.changePhoneNumber(phoneNumber);
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
