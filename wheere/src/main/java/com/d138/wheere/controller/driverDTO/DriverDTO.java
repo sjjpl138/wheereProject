9c2fec4c035f3b137365f096e4473d51df7b0c8e
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-17 22:32:56 +0900

    Controller 수정
    memberController 수정
    DTO 추가 및 수정

--- wheere/src/main/java/com/d138/wheere/controller/driverDTO/DriverDTO.java
+++ wheere/src/main/java/com/d138/wheere/controller/driverDTO/DriverDTO.java
@@ -1,10 +1,11 @@
-package com.d138.wheere.controller;
+package com.d138.wheere.controller.driverDTO;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class DriverDTO {
-    private Long did;
+    private String did;
     private String dname;
+    private String bnumber;
 }
