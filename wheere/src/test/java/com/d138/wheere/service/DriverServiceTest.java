c2503bab6f7964a93fe1849f02eb38d6991da58e
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-10-22 23:24:47 +0900

    DriverServiceTest 완료

--- wheere/src/test/java/com/d138/wheere/service/DriverServiceTest.java
+++ wheere/src/test/java/com/d138/wheere/service/DriverServiceTest.java
@@ -1,7 +1,10 @@
 package com.d138.wheere.service;
 
 import com.d138.wheere.domain.Bus;
+import com.d138.wheere.domain.BusState;
 import com.d138.wheere.domain.Driver;
+import com.d138.wheere.domain.Member;
+import com.d138.wheere.repository.BusRepository;
 import com.d138.wheere.repository.DriverRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,6 +30,12 @@ public class DriverServiceTest {
 
     @Autowired
     private DriverRepository driverRepository;
+
+    @Autowired
+    BusRepository busRepository;
+
+    private Long mId = 0L;
+    private Long bId = 0L;
 
     @Test
     public void 가입_조회() {
@@ -91,6 +102,30 @@ public class DriverServiceTest {
     }
 
     @Test
+    public void findDriverByBusTest() {
+
+        // Given
+        Bus bus1 = new Bus();
+        bus1.setId(1L);
+        em.persist(bus1);
+
+        Driver driver = new Driver();
+        driver.setId("1L");
+        driver.setRatingScore(0);
+        driver.setBus(bus1);
+        driver.setRatingCnt(0);
+        driver.setName("정영한");
+        em.persist(driver);
+
+        //  When
+        Driver findDriver = driverService.findDriverByBus(bus1.getId());
+
+        // Then
+        assertThat(findDriver.getId()).isEqualTo("1L");
+        assertThat(findDriver.getName()).isEqualTo("정영한");
+    }
+
+    @Test
     public void 평점_반영() {
 
         // Given
@@ -119,5 +154,62 @@ public class DriverServiceTest {
         // Then
         assertThat(findDriver.getRatingScore()).isEqualTo(4.0);
 
+    }
+
+    @Test
+    public void 평점_확인() {
+
+        // Given
+        String memberId1 = createMember("정영한");
+        Long busId1 = createBus("191-5");
+
+        em.flush();
+        em.clear();
+
+        Bus findBus = busRepository.findOne(busId1);
+
+        Driver driver = new Driver();
+        driver.setId("driver1");
+        driver.setName("홍길동");
+        driver.setBus(findBus);
+        driver.setRatingCnt(0);
+        driver.setRatingScore(0);
+        em.persist(driver);
+
+        // When
+        driverService.reflectScores(busId1, 4.0);
+
+        // Then
+        double rating = driverService.checkRating(driver.getId());
+
+        assertThat(rating).isEqualTo(4.0);
+
+    }
+
+    private String createMember(String name) {
+        Member member = new Member();
+        member.setId((mId++).toString());
+        member.setName(name);
+        member.setAge(22);
+        member.setPhoneNumber("010-1111-1111");
+        member.setSex("female");
+
+        em.persist(member);
+        return member.getId();
+    }
+
+    private Long createBus(String busNum) {
+        Bus bus = new Bus();
+        bus.setId(bId++);
+        bus.setBusNumber(busNum);
+        bus.setTotalWheelChairSeats(2);
+        bus.setLeftWheelChairSeats(2);
+        bus.setDirection(BusState.FORWARD);
+        bus.setBusAllocationSeq(1);
+        bus.setDepartureTime(LocalTime.now().plusMinutes(10));
+        bus.setRoutes(null);
+        em.persist(bus);
+
+        return bus.getId();
     }
 }
