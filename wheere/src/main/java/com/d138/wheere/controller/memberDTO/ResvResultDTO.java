107593ba53e0c3d36bf6ccc724ff4f8a49f2c543
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-25 00:39:04 +0900

    예약 결과 DTO 수정

--- wheere/src/main/java/com/d138/wheere/controller/memberDTO/ResvResultDTO.java
+++ wheere/src/main/java/com/d138/wheere/controller/memberDTO/ResvResultDTO.java
@@ -1,16 +1,21 @@
 package com.d138.wheere.controller.memberDTO;
 
 import com.d138.wheere.domain.Reservation;
+import com.d138.wheere.domain.ReservationState;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ResvResultDTO {
     private String  uid;
     private Long rid;
@@ -22,18 +27,11 @@ public class ResvResultDTO {
     private String rEnd;
     private boolean rIsPaid;
     private String bNumber;
+    @Enumerated(EnumType.STRING)
+    private ReservationState rState;
 
-    public ResvResultDTO(String uid, Long rid, LocalDate rTime, String rStart, String rEnd, boolean rIsPaid, String bNumber) {
-        this.uid = uid;
-        this.rid = rid;
-        this.rTime = rTime;
-        this.rStart = rStart;
-        this.rEnd = rEnd;
-        this.rIsPaid = rIsPaid;
-        this.bNumber = bNumber;
-    }
 
     public static  ResvResultDTO createResResult(String uId, boolean isPaid, Reservation resv) {
-        return new ResvResultDTO(uId, resv.getId(), resv.getReservationDate(), resv.getStartPoint(), resv.getEndPoint(), isPaid, resv.getBus().getBusNumber());
+        return new ResvResultDTO(uId, resv.getId(), resv.getReservationDate(), resv.getStartPoint(), resv.getEndPoint(), isPaid, resv.getBus().getBusNumber(), resv.getReservationState());
     }
 }
