6c1365dbc4053f2ed0a7c9b5aaff4c54b11cdd1c
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-25 19:24:20 +0900

    driver controller 수정
    driver 기능별 파라미터 수정
    버스 기사 평점 JSONObject 사용 (주석 처리, dependency 추가 필요)

--- wheere/src/main/java/com/d138/wheere/controller/DriverController.java
+++ wheere/src/main/java/com/d138/wheere/controller/DriverController.java
@@ -8,6 +8,7 @@ import com.d138.wheere.domain.Reservation;
 import com.d138.wheere.service.DriverService;
 import com.d138.wheere.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class DriverController {
     private final ReservationService reservationService;
 
     @PostMapping("/{dId}")
-    public ResponseEntity<String> signUpDriver(@PathVariable("dId")String driverId, @RequestParam("dName")String dName) {
+    public ResponseEntity<String> signUpDriver(@PathVariable("dId")String driverId,  String dName) {
 
         Driver driver = new Driver();
         driver.setId(driverId);
@@ -42,56 +43,64 @@ public class DriverController {
 
     //로그인
     @PostMapping("/{dId}/login")
-    public  ResponseEntity logInDriver(@PathVariable("dId") String driverId) {
+    public  ResponseEntity logInDriver(@PathVariable("dId") String driverId, String bNumber) {
         Driver findDriver = driverService.findDriver(driverId);
+        //bNumber랑 did를 통해서 bid 받아오기
+        //버스 변경
+        //driverService.changeBus(driverId, busId);
+
         if (findDriver == null)
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
 
         DriverDTO driverDTO = new DriverDTO();
         driverDTO.setDId(findDriver.getId());
         driverDTO.setDName(findDriver.getName());
-        driverDTO.setBNumber(findDriver.getBus().getBusNumber());
+        driverDTO.setBNumber(bNumber);
 
         return new ResponseEntity(driverDTO, HttpStatus.OK);
     }
 
     //예약 결과 조회
     @GetMapping("/resvs")
-    public List<ResvResultDTO> resvResultByDriver (@PathVariable("dId") String driverId) {
+    public List<ResvResultDTO> resvResultByDriver (@RequestParam String dId) {
 
-        Driver findDriver = driverService.findDriver(driverId);
+        Driver findDriver = driverService.findDriver(dId);
         List<Reservation> reservationsByDriver = reservationService.findReservationsByBus(findDriver.getBus().getId());
 
         List<ResvResultDTO> resvResult = new ArrayList<>();
+
         for (int i = 0; i < reservationsByDriver.size(); i++) {
             Reservation r =reservationsByDriver.get(i);
             //isPaid 임의로 true
-            ResvResultDTO resvResultDTO = ResvResultDTO.createResResult(driverId, true,  r);
+            ResvResultDTO resvResultDTO = ResvResultDTO.createResResult(dId, true,  r);
             resvResult.add(resvResultDTO);
         }
+
         return resvResult;
     }
 
     //버스 기사 평점
-    @GetMapping("/rate")
-    public double searchRatingResult(@RequestParam("dId") String driverId) {
-        Driver findDriver = driverService.findDriver(driverId);
+//    @GetMapping("/rate")
+//    public String searchRatingResult(@RequestParam String dId) {
+//
+//        double rateResult = driverService.checkRating(dId);
+//        JSONObject response = new JSONObject();
+//        response.put("rRatingScore", rateResult );
+//
+//        return response.toJSONString();
+//    }
 
-        double rateResult = driverService.checkRating(driverId);
-
-        return findDriver.getRatingScore();
-    }
-
+    //버스 기사 예약 취소
     @PostMapping("/resv/{dId}")
-    public ResponseEntity cancelResvByDriver(@PathVariable("dId")Long driverId, @RequestParam("rId")Long resvId) {
-        Reservation resv = reservationService.findReservation(resvId);
+    public ResponseEntity cancelResvByDriver(@PathVariable("dId")Long driverId, @RequestParam("rId")Long rId) {
+        Reservation resv = reservationService.findReservation(rId);
 
         DriverCancelResultDTO cancelResult = new DriverCancelResultDTO();
         cancelResult.setBId(resv.getBus().getId());
-        cancelResult.setRId(resvId);
+        cancelResult.setRId(rId);
 
         if (resv.canCancel()) {
-            reservationService.cancelReservation(resvId);
+            reservationService.cancelReservation(rId);
             return new ResponseEntity(cancelResult, HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.BAD_REQUEST);
