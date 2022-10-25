3dc2cd4890eb8b0795e2f5ce10f800c47571d93f
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-25 19:15:55 +0900

    버스 기사 평점 DTO 생성
    일반 사용자가 버스 기사 평점 매길 때 받아오는 정보

---
+++ wheere/src/main/java/com/d138/wheere/controller/memberDTO/RateDriverDTO.java
@@ -0,0 +1,12 @@
+package com.d138.wheere.controller.memberDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RateDriverDTO {
+    private String uId;
+    private String dId;
+    private Long rate;
+}
