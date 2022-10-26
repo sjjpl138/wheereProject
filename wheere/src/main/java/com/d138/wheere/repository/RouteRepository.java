67db0ccd0a733ae34d05ce6e761936f6febc02c2
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-10-27 00:12:04 +0900

    findBusRoute N + 1 문제 해결
    버스 번호로 경로 조회하는 findbUsRoute 메서드 수정
    
    findBusRouteByBusInfo 메서드 파라미터명 변경

--- wheere/src/main/java/com/d138/wheere/repository/RouteRepository.java
+++ wheere/src/main/java/com/d138/wheere/repository/RouteRepository.java
@@ -14,18 +14,20 @@ public class RouteRepository {
 
     private final EntityManager em;
 
+    // N + 1 문제 발생
     public List<Route> findBusRoute(Long busId) {
 
-        return em.createQuery("select r from Route r join r.bus b on b.id = :bId order by r.stationSeq", Route.class)
+        return em.createQuery("select r from Route r join fetch r.station join r.bus b on b.id = :bId order by r.stationSeq", Route.class)
                 .setParameter("bId", busId)
                 .getResultList();
     }
 
-    public List<Route> findBusRouteByBusInfo(String busNum, BusState busState) {
+    // 버스 번호와 버스 방향으로 해당 버스 경로 조회
+    public List<Route> findBusRouteByBusInfo(String busNum, BusState direction) {
 
         return em.createQuery("select r from Route r join fetch r.station join r.bus b on b.busNumber = :bNum and b.direction = :bDir and b.busAllocationSeq = 1 order by r.stationSeq", Route.class)
                 .setParameter("bNum", busNum)
-                .setParameter("bDir", busState)
+                .setParameter("bDir", direction)
                 .getResultList();
     }
 }
