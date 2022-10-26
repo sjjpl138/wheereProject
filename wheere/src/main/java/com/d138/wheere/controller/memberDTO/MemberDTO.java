4387c835bcc84759ad0f20ba0022311e94f8fc92
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-26 11:17:57 +0900

    memberDTO 생년월일 필드 추가
    성별 필드 삭제

--- wheere/src/main/java/com/d138/wheere/controller/memberDTO/MemberDTO.java
+++ wheere/src/main/java/com/d138/wheere/controller/memberDTO/MemberDTO.java
@@ -2,12 +2,16 @@ package com.d138.wheere.controller.memberDTO;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class MemberDTO {
     private String uName;
-    private int uAge;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate uBirthDate;
     private String  uSex;
     private String uNum;
 
