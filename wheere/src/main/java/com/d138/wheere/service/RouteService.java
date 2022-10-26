5d9322642e973a4a421e542227ae8f6ed7e6672e
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-10-26 11:03:25 +0900

    RouteService 생성

--- wheere/src/main/java/com/d138/wheere/service/RouteService.java
+++ wheere/src/main/java/com/d138/wheere/service/RouteService.java
@@ -1,10 +1,13 @@
 package com.d138.wheere.service;
 
+import com.d138.wheere.domain.BusState;
+import com.d138.wheere.domain.Route;
 import com.d138.wheere.repository.RouteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -12,4 +15,9 @@ import javax.transaction.Transactional;
 public class RouteService {
 
     private final RouteRepository routeRepository;
+
+    // 버스 번호와 버스 방향으로 버스 노선 조회
+    private List<Route> findBusRouteByBusInfo(String busNum, BusState busState) {
+        return routeRepository.findBusRouteByBusInfo(busNum, busState);
+    }
 }
