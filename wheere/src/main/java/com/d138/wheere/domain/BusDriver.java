9ee248cd52683b800bceea01f0f49cd9e4ee6630
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-18 23:37:06 +0900

    BusDriver 엔티티 생성

--- wheere/src/main/java/com/d138/wheere/domain/BusDriver.java
+++ wheere/src/main/java/com/d138/wheere/domain/BusDriver.java
@@ -1,13 +1,13 @@
 package com.d138.wheere.domain;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Getter
+// TODO (연관관계 설정하고 버스기사 평점 메기기 기능 추가)
 public class BusDriver {
 
     @Id
@@ -22,6 +22,7 @@ public class BusDriver {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "DRIVER_ID")
     private Driver driver;
-
+    
+    // 버스 운행 날짜
     private LocalDate operationDate;
 }
