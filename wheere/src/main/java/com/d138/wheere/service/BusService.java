2cbc8dc0ec6f72c034023de356f8d2f346cfecd0
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-19 21:31:57 +0900

    BusService 수정 & TODO 작성

--- wheere/src/main/java/com/d138/wheere/service/BusService.java
+++ wheere/src/main/java/com/d138/wheere/service/BusService.java
@@ -2,10 +2,9 @@ package com.d138.wheere.service;
 
 import com.d138.wheere.domain.Bus;
 import com.d138.wheere.domain.BusState;
-import com.d138.wheere.domain.Reservation;
-import com.d138.wheere.repository.BusNumDirDTO;
+import com.d138.wheere.repository.bus.query.BusNumDirDTO;
 import com.d138.wheere.repository.BusRepository;
-import com.d138.wheere.repository.ReservationRepository;
+import com.d138.wheere.repository.bus.query.BusQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +19,7 @@ public class BusService {
 
     private final BusRepository busRepository;
 
-    private final ReservationRepository reservationRepository;
+    private final BusQueryRepository busQueryRepository;
 
     // 사용자 또는 버스 기사가 버스 번호, 방향, 출발 시간으로 버스 선택
     // 버스 예약, 버스 배차에 사용됨
@@ -28,9 +27,17 @@ public class BusService {
         return busRepository.findBus(busNum, busState, departureTime);
     }
 
-    // 버스 번호, 방향만 조회하는 메서드
+    // TODO (BusID로 버스 조회할 필요가 없는가?)
+
+
+    /**
+     * 버스 번호, 방향만 조회하는 메서드
+     * 버스 기사가 로그인 후 자신의 버스 배차를 변경하고자 할 때 조회용으로 사용
+     *
+     * @return 버스 번호와 방향 List
+     */
     public List<BusNumDirDTO> inquireBusNumDir() {
-        return busRepository.findBusNumDir();
+        return busQueryRepository.findBusNumDir();
     }
 
     // 버스 시간표 조회 메서드
