9ccb4160b2cb122279e4b0fb84d21e9fbf15871e
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-19 21:30:51 +0900

    BusDriver 수정

--- wheere/src/main/java/com/d138/wheere/domain/BusDriver.java
+++ wheere/src/main/java/com/d138/wheere/domain/BusDriver.java
@@ -1,12 +1,14 @@
 package com.d138.wheere.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Getter
+@NoArgsConstructor
 // TODO (연관관계 설정하고 버스기사 평점 메기기 기능 추가)
 public class BusDriver {
 
@@ -25,4 +27,15 @@ public class BusDriver {
     
     // 버스 운행 날짜
     private LocalDate operationDate;
+
+    public BusDriver(Bus bus, Driver driver, LocalDate operationDate) {
+        this.bus = bus;
+        this.driver = driver;
+        this.operationDate = operationDate;
+    }
+
+    //== 생성 메서드 ==//
+    public static BusDriver createBusDriver(Bus bus, Driver driver, LocalDate operationDate) {
+        return new BusDriver(bus, driver, operationDate);
+    }
 }
