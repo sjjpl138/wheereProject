edf186765444ba4b9afc992aff828a94077d515d
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-19 21:30:41 +0900

    BusRepository 수정
    findBusNumDir 메서드는 새로운 패키지에 작성

--- wheere/src/main/java/com/d138/wheere/repository/BusRepository.java
+++ wheere/src/main/java/com/d138/wheere/repository/BusRepository.java
@@ -15,31 +15,30 @@ public class BusRepository {
 
     private final EntityManager em;
 
-    public Long save(Bus bus) {
-        em.persist(bus);
-
-        return bus.getId();
-    }
-
+    /**
+     * 버스 ID (PK)로 버스 조회
+     * @param busId
+     * @return
+     */
     public Bus findOne(Long busId) {
 
         return em.find(Bus.class, busId);
     }
 
-    // 사용자 또는 버스 기사가 버스 번호, 방향, 출발 시간으로 버스 선택
-    // 버스 예약, 버스 배차에 사용됨
+    /**
+     * 버스 번호, 방향, 출발 시간으로 버스 조회
+     * 버스 예약, 버스 배차에 사용됨
+     * @param busNum 버스 번호
+     * @param busState 버스 방향 (정방향, 역방향)
+     * @param departureTime (출발 시간)
+     * @return 조회하려는 버스
+     */
     public Bus findBus(String busNum, BusState busState, LocalTime departureTime) {
         return em.createQuery("select b from Bus b where b.busNumber = :busNum and b.direction = :busState and b.departureTime = :departureTime", Bus.class)
                 .setParameter("busNum", busNum)
                 .setParameter("busState", busState)
                 .setParameter("departureTime", departureTime)
                 .getSingleResult();
-    }
-
-    // 버스 번호, 방향만 조회하는 메서드
-    public List<BusNumDirDTO> findBusNumDir() {
-        return em.createQuery("select new com.d138.wheere.repository.BusNumDirDTO(b.busNumber, b.direction) from Bus b where b.busAllocationSeq = 1 order by (b.busNumber, b.direction)", BusNumDirDTO.class)
-                .getResultList();
     }
 
     // 버스 출발 시간 조회 메서드
