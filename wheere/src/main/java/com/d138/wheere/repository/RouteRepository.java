d1fd5f46eb3c89fa5f2227da9d97a045a3160e82
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-20 17:16:13 +0900

    RouteRepository 수정

--- wheere/src/main/java/com/d138/wheere/repository/RouteRepository.java
+++ wheere/src/main/java/com/d138/wheere/repository/RouteRepository.java
@@ -14,30 +14,21 @@ public class RouteRepository {
 
     private final EntityManager em;
 
-    // 버스 번호와 버스 방향으로 해당 버스 경로 조회
-    public List<Route> findBusRouteByBusInfo(String busNum, BusState direction) {
-
-        return em.createQuery("select r from Route r join fetch r.station join r.bus b on b.busNumber = :bNum and b.direction = :bDir and b.busAllocationSeq = 1 order by r.stationSeq", Route.class)
-                .setParameter("bNum", busNum)
-                .setParameter("bDir", direction)
-                .getResultList();
-    }
-
     /**
-     * 버스 ID (PK), 출발 정류장 순번, 도착 정류장 순번으로 Route 조회
-     * 예약할 때 JSON 스펙으로 정류장 순번 받아올 때 호출됨
-     * @param busId
-     * @param startSeq
-     * @param endSeq
+     * 버스 번호와 버스 방향으로 해당 버스 경로 조회
+     * @param busNum
+     * @param direction
      * @return
      */
-    public List<Route> findRoutesBySeq(Long busId, int startSeq, int endSeq) {
-        return em.createQuery("select r from Route r"
-                        + " join r.bus b on b.id = :busId"
-                        + " join fetch r.station", Route.class)
-                .setParameter("busId", busId)
-                .setFirstResult(startSeq-1)
-                .setMaxResults(endSeq - startSeq + 1)
+    public List<Route> findBusRouteByBusInfo(String busNum, BusState direction) {
+
+        return em.createQuery("select r from Route r" +
+                        " join fetch r.station join r.bus b on b.busNumber = :bNum" +
+                        " and b.direction = :bDir" +
+                        " and b.busAllocationSeq = 1" +
+                        " order by r.stationSeq", Route.class)
+                .setParameter("bNum", busNum)
+                .setParameter("bDir", direction)
                 .getResultList();
     }
 
@@ -45,6 +36,7 @@ public class RouteRepository {
      * 버스 ID (PK), 정류장 이름으로 Route 조회
      * 예약할 때 JSON 스펙으로 정류장 이름을 받아오면 해당 메서드 호출
      * 아니면 findRouteBySeq 호출
+     *
      * @param busId
      * @param stationName
      * @return
@@ -56,5 +48,38 @@ public class RouteRepository {
                 .setParameter("busId", busId)
                 .setParameter("stationName", stationName)
                 .getSingleResult();
+    }
+
+    /**
+     * 출발, 도착 정류장 조회
+     * 버스 예약할 때 사용됨
+     * @param busId
+     * @param seqList
+     * @return 출발, 도착 정류장
+     */
+    public List<Route> findRoutesByBusAndSeq(Long busId, List<Integer> seqList) {
+        return em.createQuery("select r from Route r"
+                        + " join r.bus b on b.id = :busId"
+                        + " join fetch r.station s"
+                        + " where r.stationSeq in :seqList", Route.class)
+                .setParameter("busId", busId)
+                .setParameter("seqList", seqList)
+                .getResultList();
+    }
+
+    /**
+     * 출발 정류장, 도착 정류장에 대한 정류장 순번 얻기
+     * 예약 취소 (CANCEL) 시 호출됨
+     * @param busId
+     * @param pointList
+     * @return
+     */
+    public List<Integer> findSeqByBusAndName(Long busId, List<String> pointList) {
+        return em.createQuery("select r.stationSeq from Route r"
+                        + " join r.bus b on b.id = :busId"
+                        + " where r.station.name in :pointList", Integer.class)
+                .setParameter("busId", busId)
+                .setParameter("pointList", pointList)
+                .getResultList();
     }
 }
