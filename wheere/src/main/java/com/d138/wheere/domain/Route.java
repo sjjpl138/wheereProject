056e4081be77e5540fd85b19d84e6afe65c9dff5
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-20 17:15:38 +0900

    Route 수정

--- wheere/src/main/java/com/d138/wheere/domain/Route.java
+++ wheere/src/main/java/com/d138/wheere/domain/Route.java
@@ -33,20 +33,12 @@ public class Route {
     // 예상 도착 시간 (from 이전 정류장)
     private LocalTime arrivalTime;
 
-    // 예약 가능한 총 좌석 수
-    private int totalSeatsNum;
-
-    // 예약 가능한 남은 좌석 수
-    private int leftSeatsNum;
-
-    public Route(Long id, Bus bus, Station station, int stationSeq, LocalTime arrivalTime, int totalSeatsNum, int leftSeatsNum) {
+    public Route(Long id, Bus bus, Station station, int stationSeq, LocalTime arrivalTime) {
         this.id = id;
         setBus(bus);
         this.station = station;
         this.stationSeq = stationSeq;
         this.arrivalTime = arrivalTime;
-        this.totalSeatsNum = totalSeatsNum;
-        this.leftSeatsNum = leftSeatsNum;
     }
 
     /* 연관관계 편의 메서드 */
@@ -56,21 +48,5 @@ public class Route {
         }
         this.bus = bus;
         bus.getRoutes().add(this);
-    }
-
-    /* 비지니스 로직 */
-    public void addSeats() {
-        int restSeats = this.leftSeatsNum + 1;
-        if (restSeats <= totalSeatsNum) {
-            this.totalSeatsNum = restSeats;
-        }
-    }
-
-    public void subSeats() {
-        int restSeats = this.leftSeatsNum - 1;
-        if (restSeats < 0) {
-            throw new NotEnoughSeatsException("남은 좌석이 없습니다.");
-        }
-        this.leftSeatsNum = restSeats;
     }
 }
