f18595b7264e5947d08bce489e8b624cdaa16aad
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-11-20 17:16:31 +0900

    ReservationService 수정

--- wheere/src/main/java/com/d138/wheere/service/ReservationService.java
+++ wheere/src/main/java/com/d138/wheere/service/ReservationService.java
@@ -1,16 +1,14 @@
 package com.d138.wheere.service;
 
 import com.d138.wheere.domain.*;
-import com.d138.wheere.repository.BusRepository;
-import com.d138.wheere.repository.MemberRepository;
-import com.d138.wheere.repository.ReservationRepository;
-import com.d138.wheere.repository.RouteRepository;
+import com.d138.wheere.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,8 +19,8 @@ public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final MemberRepository memberRepository;
     private final BusRepository busRepository;
-
     private final RouteRepository routeRepository;
+    private final SeatRepository seatRepository;
 
     /**
      * 예약 생성
@@ -41,16 +39,16 @@ public class ReservationService {
         Member member = memberRepository.findOne(memberId);
         Bus bus = busRepository.findOne(busId);
 
-        // 출발 정류장, 도착 정류장 순번
-        List<Route> findRoutes = routeRepository.findRoutesBySeq(busId, startSeq, endSeq);
+        // 출발 정류장, 도착 정류장 조회
+        List<Route> findRoutes = getStartEndRoutes(busId, startSeq, endSeq);
 
+        // 출발 정류장
         Route startRoute = findRoutes.get(0);
-        Route endRoute = findRoutes.get(findRoutes.size() - 1);
+        // 도착 정류장
+        Route endRoute = findRoutes.get(1);
 
         /* 버스 제약사항 추가 */
-
         // 예약하려는 버스 출발 시간이 현재 시간 이전이라면 예약 불가
-        // TODO (버스 출발 시간이 아닌 예상 도착 시간으로 비교하기)
         compareBusDepartureTime(startRoute, reservationDate);
 
         // 동일 버스에 대한 기존 예약이 존재하고 기존 예약의 상태가 취소 상태가 아니라면 예약 불가
@@ -59,19 +57,53 @@ public class ReservationService {
         // 예약 생성
         String startPoint = startRoute.getStation().getName();
         String endPoint = endRoute.getStation().getName();
+
         Reservation reservation = Reservation.createReservation(member, bus, startPoint, endPoint, reservationDate);
 
         // 해당 버스 정류장별로 좌석 감소 (마지막 정류장 제외)
-        subRouteSeats(findRoutes);
+        subSeatsPerRoute(startSeq, endSeq, reservationDate, bus);
 
         reservationRepository.save(reservation);
         return reservation.getId();
     }
 
-    private void subRouteSeats(List<Route> findRoutes) {
-        for (int i = 0; i < findRoutes.size() - 1; i++) {
-            findRoutes.get(i).subSeats();
+    private void subSeatsPerRoute(int startSeq, int endSeq, LocalDate reservationDate, Bus bus) {
+        List<Integer> seqList = new ArrayList<>();
+        for(int i = startSeq; i <= endSeq; i++)
+            seqList.add(i);
+
+        List<Seat> findSeats = seatRepository.findSeatsByBusAndDate(bus.getId(), reservationDate, seqList);
+
+        // 예약하려는 버스에 대한 Seat이 생성되어 있지 않다면
+        if (findSeats.isEmpty()) {
+            createSeatPerRoute(reservationDate, bus);
+            List<Seat> seats = seatRepository.findSeatsByBusAndDate(bus.getId(), reservationDate, seqList);
+            calSubSeats(seats);
+        } else{
+            calSubSeats(findSeats);
         }
+    }
+
+    private void createSeatPerRoute(LocalDate reservationDate, Bus bus) {
+        List<Route> routes = bus.getRoutes();
+        int totalSeatsNum = 2;
+        for (Route route : routes) {
+            Seat seat = Seat.createSeat(route, totalSeatsNum, reservationDate);
+            seatRepository.save(seat);
+        }
+    }
+
+    private static void calSubSeats(List<Seat> seats) {
+        for (int i = 0; i < seats.size() - 1; i++) {
+            seats.get(i).subSeats();
+        }
+    }
+
+    private List<Route> getStartEndRoutes(Long busId, int startSeq, int endSeq) {
+        List<Integer> seqList = List.of(startSeq, endSeq);
+
+        List<Route> findRoutes = routeRepository.findRoutesByBusAndSeq(busId, seqList);
+        return findRoutes;
     }
 
     private void validateDuplicateReservation(LocalDate reservationDate, Member member, Bus bus) {
@@ -92,24 +124,55 @@ public class ReservationService {
 
     /**
      * 예약 취소
+     * @param reservationId
      */
-    // TODO [예약 상태 변경 (취소, CANCEL) 구현해야 함]
     @Transactional
     public void cancelReservation(Long reservationId) {
         // 예약 엔티티 조회
         Reservation findReservation = reservationRepository.findOne(reservationId);
 
+        Long busId = findReservation.getBus().getId();
+        LocalDate reservationDate = findReservation.getReservationDate();
+
         findReservation.cancel();
 
-        // 파라미터로 예약 날짜도 받아와 Seat 객체 찾고 버스 좌석 증가시켜야 함
-        
+        // 버스 좌석 증가
+        addSeatsPerRoute(findReservation, busId, reservationDate);
     }
 
-    // 예약 거절
-    // TODO [예약 상태 변경 (거절, REFUSED) 구현해야 함]
+    /**
+     * 예약 거절
+     * @param reservationId
+     */
     @Transactional
     public void rejectReservation(Long reservationId) {
+        // 예약 엔티티 조회
         Reservation findReservation = reservationRepository.findOne(reservationId);
+
+        Long busId = findReservation.getBus().getId();
+        LocalDate reservationDate = findReservation.getReservationDate();
+
+        findReservation.reject();
+
+        // 버스 좌석 증가
+        addSeatsPerRoute(findReservation, busId, reservationDate);
+    }
+
+    private void addSeatsPerRoute(Reservation findReservation, Long busId, LocalDate reservationDate) {
+        String startPoint = findReservation.getStartPoint();
+        String endPoint = findReservation.getEndPoint();
+
+        List<String> pointList = List.of(startPoint, endPoint);
+        List<Integer> stationSeqs = routeRepository.findSeqByBusAndName(busId, pointList);
+
+        List<Integer> seqList = new ArrayList<>();
+        for(int i = stationSeqs.get(0); i <= stationSeqs.get(1); i++)
+            seqList.add(i);
+
+        List<Seat> findSeats = seatRepository.findSeatsByBusAndDate(busId, reservationDate, seqList);
+        for (int i = 0; i < findSeats.size() - 1; i++) {
+            findSeats.get(i).addSeats();
+        }
     }
 
     // TODO [예약 상태 변경 (예약됨, RESERVED) 구현해야 함]
