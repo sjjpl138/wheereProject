ee3cfc2df823802632434c0f4598ed98e0ed3594
Jimin <77067383+wl2258@users.noreply.github.com>
2022-11-25 19:21:40 +0900

    AssignBusDTO bOperationDate 타입 변경

--- wheere/src/main/java/com/d138/wheere/controller/driverDTO/AssignBusDTO.java
+++ wheere/src/main/java/com/d138/wheere/controller/driverDTO/AssignBusDTO.java
@@ -1,16 +1,14 @@
 package com.d138.wheere.controller.driverDTO;
 
-import com.d138.wheere.domain.BusState;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 @Data
 public class AssignBusDTO {
     private String dId;
     private Long bId;
-    @DateTimeFormat(pattern = "HH:mm:ss")
-    private LocalTime bOperationDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate bOperationDate;
 }
