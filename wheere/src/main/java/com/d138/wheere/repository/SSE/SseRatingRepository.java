d0d67a237626235c198c02d96011040609670359
Jimin <77067383+wl2258@users.noreply.github.com>
2022-11-27 04:30:55 +0900

    SSE 일반 사용자 - 서버 Repository

---
+++ wheere/src/main/java/com/d138/wheere/repository/SSE/SseRatingRepository.java
@@ -0,0 +1,71 @@
+package com.d138.wheere.repository.SSE;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Repository
+@NoArgsConstructor
+@Log4j2
+public class SseRatingRepository implements SseBaseRepository{
+
+    @Override
+    public SseEmitter save(String id, SseEmitter sseEmitter) {
+        emitters.put(id, sseEmitter);
+        log.info(emitters);
+        return sseEmitter;
+    }
+
+    @Override
+    public void deleteById(String emitterId) {
+        if (!emitters.isEmpty()) {
+            emitters.remove(emitterId);
+        }
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllStartWithById(String emitterId) {
+        return emitters.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().startsWith(emitterId))
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue)
+                );
+    }
+
+    @Override
+    public void saveEventCache(String key, Object event) {
+        eventCache.put(key,  event);
+    }
+
+    @Override
+    public Map<String, Object> findAllEventCacheWithId(String emitterId) {
+        return emitters.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().startsWith(emitterId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public void deleteAllEmitterWithId(String emitterId) {
+        emitters.forEach((key, emitter) -> {
+            if (key.startsWith(emitterId)){
+                emitters.remove(key);
+            }
+        });
+    }
+
+    @Override
+    public void deleteAllEventCacheStartWithId(String emitterId) {
+        eventCache.forEach((key, emitter) -> {
+            if (key.startsWith(emitterId)){
+                emitters.remove(key);
+            }
+        });
+    }
+}
